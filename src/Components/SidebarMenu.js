5510950adb8c137b430cc25cdec73cf903951f59
Shin Ho Jung <jshinhoxp@gmail.com>
2023-01-04 20:29:43 +0200

    added exit to SideBarMenu

--- src/Components/SidebarMenu.js
+++ src/Components/SidebarMenu.js
@@ -1,12 +1,28 @@
 import React from "react";
-import "../css/Sidebar.css"
+import "../css/Sidebar.css";
+import * as RxIcons from "react-icons/rx";
+import { MenuData } from "./MenuData";
+import { Link } from "react-router-dom";
 
-function SidebarMenu () {
-  return (
-    <div className="mobile-menu">
-      <p> Hello </p>
-    </div>
-  )
+function SidebarMenu(props) {
+   if (props.status === true ) {
+      return (
+         <div className="mobile-menu">
+            <h1> MENU <RxIcons.RxExit onClick={props.onClick} /> </h1>
+            { MenuData.map( (item, index) => {
+               return (
+                  <li key={index} className={item.cName}>
+                     <Link to={item.path} target={item.target}>
+                           {item.icon}
+                           <span> {item.title} </span>
+                     </Link>
+                  </li>
+               )
+            })}
+         </div>
+      )
+   }
 }
 
-export default SidebarMenu;
+
+export default SidebarMenu
