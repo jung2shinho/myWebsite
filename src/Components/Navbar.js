b3fae256d437390747a4deb301e392f4a071dedb
Shin Ho Jung <jshinhoxp@gmail.com>
2023-01-04 20:29:43 +0200

    added exit to SideBarMenu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,20 @@
 // Use Link instead of <a> since Link does not send request to server side, thus, faster loading.
 import React, { useState } from "react";
-import "../css/Navbar.css"
-import { Link } from "react-router-dom"
-import { MenuData } from "./MenuData"
-import SidebarMenu from "./SidebarMenu";
+import "../css/Navbar.css";
+import { Link } from "react-router-dom";
+import { MenuData } from "./MenuData";
+
 import * as BiIcons from "react-icons/bi"
 import * as RxIcons from "react-icons/rx"
+import SidebarMenu from "./SidebarMenu";
 
 function Navbar() {
    // React Hook for clicking burger menu
-   const [status, setStatus]= useState(true) // initial value
+   const [status, setStatus]= useState(false) // initial value
    
    const menuClick = () => {
-      setStatus(!status) // switches value between true and false
+      setStatus(status => !status) // switches value between true and false
       console.log(status)
-      if (status == true) {
-         <SidebarMenu />
-      } else {}
    }
 
    return (
@@ -25,17 +23,20 @@ function Navbar() {
                <BiIcons.BiCode />
                SEAN JUNG
          </Link>
+         
          { MenuData.map( (item, index) => {
-               return (
-                  <li key={index} className={item.cName}>
-                     <Link to={item.path} target={item.target}>
-                           {item.icon}
-                           <span> {item.title} </span>
-                     </Link>
-                  </li>
-               )
-         })}
+            return (
+            <li key={index} className={item.cName}>
+               <Link to={item.path} target={item.target}>
+                     {item.icon}
+                     <span> {item.title} </span>
+               </Link>
+            </li>
+            )})
+         }
+         
          <RxIcons.RxHamburgerMenu onClick={menuClick} className="burger" />
+         <SidebarMenu status={status} onClick={menuClick} />
       </div>
    )
 }
