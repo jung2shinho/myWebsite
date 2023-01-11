477a33164aaf2fcf28378fa484bbe3498e610f32
Shin Ho Jung <jshinhoxp@gmail.com>
2023-01-11 20:29:44 +0200

    added backend with Get/Post function

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js'; 
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json()); // indicates that use of json objects
+
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => { // sending a get request to our routes
+   console.log('[TEST]!');
+   res.send('Hello from Homepage.');
+} ) 
+
+
+app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`))
