fa5f1248bf4a93a73c53f96e4e50af8e0023fe84
Shin Ho Jung <jshinhoxp@gmail.com>
2023-01-11 20:29:44 +0200

    added backend with Get/Post function

---
+++ backend/routes/users.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const router = express.Router();
+
+const users = [
+   {
+      firstName: "John",
+      lastName: "Doe",
+      age: 25
+   },
+   {
+      firstName: "Jane",
+      lastName: "Doe",
+      age: 24
+   }
+]
+
+// all routes in here are starting with /users
+router.get('/', (req, res) => {
+   console.log(users);
+   res.send(users);
+});
+
+router.post('/', (req, res) => {
+   const user = req.body;
+   users.push(user);
+
+   res.send(`User with the username ${user.firstName} added to database`);
+})
+
+export default router;
