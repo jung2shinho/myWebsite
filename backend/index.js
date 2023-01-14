2112773a471d1102b280b01aa35ac2d3dbd6c216
Shin Ho Jung <jshinhoxp@gmail.com>
2023-01-14 13:54:07 +0200

    successfully connected to MongoDB database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,9 @@
 // “index.js”: The main entry point for the Express server and configuration initialization
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const usersRoutes = require ('./routes/users.js');
+const dotenv = require('dotenv').config();
 
 // init middleware express server
 const app = express();
@@ -12,20 +12,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => 
    console.log(`Server Running on port: http://localhost:${PORT}`)
 )
-
 app.use(bodyParser.json()); // indicates that use of json objects
 
-
-// Connect to DB
-mongoose.connect(
-   process.env.ATLAS_URI,
-   {useNewUrlParser: true }, 
-   () => console.log('Connected to DB')
-);
-
 // Route for homepage
-app.get('/', (req, res) => { // sending a get request to our routes
-   console.log('[TEST]!');
+app.get('/', (req, res) => {
    res.send('Hello from Homepage.');
 } ) 
 
