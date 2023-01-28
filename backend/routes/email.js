9faf8dbf11fdf96fc18afdbaa7b836ae15c25740
Shin Ho Jung <jshinhoxp@gmail.com>
2023-01-28 19:29:11 +0200

    corrected input functions for routes

--- backend/routes/email.js
+++ backend/routes/email.js
@@ -1,11 +1,12 @@
 // “routes/user.js”: Exposes the REST API endpoints and performs their business logic against the Atlas cluster.
 const express = require('express');
-const { getIndex } = require("../controllers/index.js")
+const { getEmail, createEmail, getEmailId,
+   deleteEmail, updateEmail } = require("../controllers/email.js")
 
 const router = express.Router();
 
 // all routes in here are starting with /users
-router.get('/', getIndex)
+router.get('/', getEmail)
 router.post('/', createEmail)
 router.get('/:id', getEmailId)
 router.delete('/:id', deleteEmail)
