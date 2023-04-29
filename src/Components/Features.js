f0dc57d9638721eab2f1085b0751d119ff336c1e
Shin Ho Jung <jshinhoxp@gmail.com>
2023-04-29 14:10:39 +0300

    added @react-three/fiber & drei

--- src/Components/Features.js
+++ src/Components/Features.js
@@ -1,20 +1,27 @@
 import React from 'react'
 
 import { Routes, Route } from 'react-router-dom';
+import Home from '../Pages/Home';
 import About from '../Pages/About';
 import Contact from '../Pages/Contact';
 import Projects from '../Pages/Projects';
 import Resume from '../Pages/Resume';
 import Blog from '../Pages/Blog';
 
+import { Canvas } from '@react-three/fiber';
+
 import Redirect from '../Components/Redirect';
 import ML from '../Pages/ML';
+import Experience from './Experience';
 
 function Features () { 
   return (
     <div>
     <Routes>
-          <Route path="/" element={<About/>} />
+          <Route path="/" element={
+            <Canvas>
+              <Experience />
+            </Canvas>} />  
           <Route path="/About" element={<About />} />
           <Route path="/Contact" element={<Contact />} />
           <Route path="/Projects" element={<Projects />} />
