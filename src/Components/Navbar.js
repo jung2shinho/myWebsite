76e339fa57486b97567320686b4d3ebc1823546f
Shin Ho Jung <sxjung@uw.edu>
2023-09-24 13:12:32 -0700

    added 3D Home Page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -19,7 +19,7 @@ function Navbar() {
    return (
       <div className="navbar">
          <Link to ="/" className="site-title">
-               SEAN JUNG
+               SHINHO JUNG
          </Link>
          
          { MenuData.map( (item, index) => {
